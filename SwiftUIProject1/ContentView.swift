4032c423e0b5d547156175a6671960c5441424ce
Michael Miles <michael.gage.miles@gmail.com>
2019-07-08 09:30:34 -0700

    Completed Project

--- SwiftUIProject1/ContentView.swift
+++ SwiftUIProject1/ContentView.swift
@@ -7,10 +7,58 @@
 //
 
 import SwiftUI
+import Combine
+
+class DataSource: BindableObject {
+    let didChange = PassthroughSubject<Void, Never>()
+    var pictures = [String]()
+    
+    init() {
+        let fm = FileManager.default
+        
+        if let path = Bundle.main.resourcePath, let items = try? fm.contentsOfDirectory(atPath: path) {
+            for item in items {
+                if item.hasPrefix("nssl") {
+                    pictures.append(item)
+                }
+            }
+        }
+        didChange.send(())
+    }
+}
+
+struct DetailView: View {
+    //State stores the data for local variables
+    @State var hidesNavBar = false
+    var selectedImage: String
+    
+    var body: some View {
+        let img = UIImage(named: selectedImage)!
+        return Image(uiImage: img)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .navigationBarTitle(Text(selectedImage), displayMode: .inline)
+            .navigationBarHidden(hidesNavBar)
+            .tapAction {
+                self.hidesNavBar.toggle()
+        }
+    }
+}
 
 struct ContentView : View {
+    //ObjectBinding stores the data, since SwiftUI is structs
+    @ObjectBinding var dataSource = DataSource()
+    
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            List(dataSource.pictures.identified(by: \.self)) { picture in
+                NavigationLink(
+                    destination: DetailView(selectedImage: picture),
+                    isDetail: true) {
+                        Text(picture)
+                }
+            }.navigationBarTitle("Storm Viewer")
+        }
     }
 }
 
